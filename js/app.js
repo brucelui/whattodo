20a1d21de6fed1361de1eead54f3de1f81a5aea7
brucelui <buru@buru-PRO.local>
2015-02-23 16:36:19 -0800

    initial upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+// var quizApp = angular.module('quizApp', ['ngRoute']);
+
+
+// quizApp.config(function($routeProvider) {
+//   //set up routes
+//   $routeProvider
+//     .when('/', {
+//       templateUrl: 'app_pages/main.html',
+//       controller: 'mainController'
+//     })
+
+//     .when('/type', {
+//       templateUrl: 'app_pages/quizA_start.html'
+//     })
+
+//     .when('/q1', {
+//       templateUrl: 'app_pages/quizA_1.html',
+//       controller: 'quizQ1Controller'
+//     })
+
+//     .when('/result', {
+
+//       templateUrl: 'app_pages/quizA_result.html'
+//     })
+
+//     .otherwise( {
+//     	redirectTo: '/'
+//     })
+
+// });
---
+++ js/controllers.js
@@ -0,0 +1,20 @@
+var currentSum = 0;
+var currentQuestion = 0;
+
+
+/*
+var pokeController = angular.module('pokeController',[]);
+
+pokeApp.controller('quizCtrl', ['$scope', function($scope) {
+  /* Stores Answer Value in Array*/ /*
+  var output = 0;
+  
+
+}]);
+
+*/
+
+
+
+
+  
